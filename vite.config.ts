ca70270503d88a8959d879ffa502533c728b5fd0
Ann 杏 Kilzer <akilzer@gmail.com>
2023-06-19 21:09:43 +0900

    install vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom', 
+    setupFiles: 'src/setupTests.ts',
+    coverage: {
+      enabled: true,
+      provider: 'istanbul',
+      all: true
+    }
+  }
 })
