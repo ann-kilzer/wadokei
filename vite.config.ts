d507dfbfbbf7e2de8c96a1702e80de5a48405727
Ann 杏 Kilzer <akilzer@gmail.com>
2024-07-27 01:23:12 +0900

    set vite base

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom', 
-    setupFiles: 'src/setupTests.ts',
-    coverage: {
-      enabled: true,
-      provider: 'istanbul',
-      all: true
+    plugins: [react()],
+    base: '/',
+    test: {
+        environment: 'jsdom',
+        setupFiles: 'src/setupTests.ts',
+        coverage: {
+            enabled: true,
+            provider: 'istanbul',
+            all: true
+        }
     }
-  }
 })
